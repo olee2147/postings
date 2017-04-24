fcbb90d6718259ee6c4839a3e9c566a0cf45f491
Eunseok (Oscar) Lee <olee2147@likelion.org>
2017-04-24 12:30:58 +0000

    Posting and Comment added to a specific Posting ID

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,4 +1,17 @@
 class PostingsController < ApplicationController
     def index
+      @all_postings = Posting.all
+      @all_comment = Comment.all
     end
+    
+    def create
+      posting = Posting.new
+      posting.title = params[:title]
+      posting.body = params[:body]
+      posting.save
+    
+      redirect_to '/index'
+    end
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
     root 'postings#index'
+    get '/index' => 'postings#index'
+    
+    post '/' => 'postings#create'
+    post '/index' => 'postings#create'
+    
+    post '/comment/create' => 'comment#create'
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
